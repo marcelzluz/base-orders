00d8a8c8658adbd127f7a8cd5a82337eff9c3776
Marcel Zulian <marcelz.luz@gmail.com>
2025-06-03 16:25:58 -0300

    refactor: remover logs

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,8 +1,4 @@
 import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
 
-console.log(
-    "[MSW] Handlers carregados:",
-    handlers.map((h) => h.info?.path || h),
-);
 export const worker = setupWorker(...handlers);
